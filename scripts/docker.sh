267e9c2fde699d812ae6ddd8f17c42c5fefee329
Abdulrahman Tabaza <84684514+a-tabaza@users.noreply.github.com>
2024-05-18 20:09:59 +0300

    my build script that you get from the repo clone the repo

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,4 +1,2 @@
-git clone https://github.com/a-tabaza/fairouz_demo.git
-cd fairouz_demo
 docker build -t fairouz-demo .
 docker run -d -p 8501:8501 fairouz-demo
--- scripts/venv.sh
+++ scripts/venv.sh
@@ -1,5 +1,3 @@
-git clone https://github.com/a-tabaza/fairouz_demo.git
-cd fairouz_demo
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
